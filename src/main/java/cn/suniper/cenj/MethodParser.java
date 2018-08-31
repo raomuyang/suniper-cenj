bf154d88da0d3daa888e499e29ea5b5794fe3852
rao-mengnan <raomengnan@genedock.com>
2018-08-31 10:13:48 +0800

    add doc

--- src/main/java/cn/suniper/cenj/MethodParser.java
+++ src/main/java/cn/suniper/cenj/MethodParser.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 Suniper
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package cn.suniper.cenj;
 
 import javax.persistence.Column;
@@ -9,6 +25,10 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 
 /**
+ * Generate SQL  by {@link Interpreter}, and attach the parameters information
+ * that initial from the type of the first parameter while there are none arguments
+ * found from SQL but the count of the parameters is one.
+ *
  * @author Rao Mengnan
  *         on 2018/8/30.
  */
